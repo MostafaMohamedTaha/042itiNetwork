32cb85a57f9b0d5065ce21d1ff47f07e73dcbebb
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-07-23 15:48:11 +0300

    network and cyber1

--- netwok.js
+++ netwok.js
@@ -1,1 +1,86 @@
-//
+//todo(((((((((((((computer network)))))))))))))
+// nic network interface card \adaptor
+//hub ,switch[connect multi device]{wifi,wire} 
+//access point =>network connection receive to switch
+//ip source=>destination
+//mac address source=>destination
+//router =>connect between more than one network
+//router nic internet service provider
+// device share server 
+
+//network element
+//hardware[device],[medium=>wire,wireless,satellite]
+//software [mess,protocol]
+
+//nic internal,external =>mac(physical,)
+
+//hub,switch,repeater[network strength],access point,router
+
+//lan local area network 1000 m [high speed,under admin],wan wide area network [service =>low,isp admin]
+
+//icann dns maker internet corporation for assigned names,numbers
+
+//shape =>circle, star[switch]
+
+//peer to peer connection , server[mail,web,file,print]
+
+// layers physical,mac[data link],[network{ip,icmp,arp,rarp}],transport[tcp[delivery ensured],udp[delivery not ensure]]
+// .session,presentation,app [ftp,smtp,dns,telnet]
+// [pmn] [tspa]
+
+//osi model open system interconnect =>1983 [univeral concept]
+//osi reference breaks this into layers
+
+// !tcp transmission control protocol 
+// original
+
+// 5 layers
+//physical,data link,network,transport,app[session,presentation]
+
+//? mac byte[8bit] mac=[48bit=6byte]=>12 hexa digit
+// every bit 2 hex digit 
+
+//physical =>unicast[one to one],multicast[one to group]=>broadcast[one to all]
+
+//? max ipconfig /all |renew |release  getMac /v mac address =>dhcp
+
+//? ip [4byte,32bit]=>[from binary=>decimal]192.168.1.1
+
+//? private class a[local ip 2],b,c ,public ip 255.255.255.254
+
+//? nating is public same ip for local ip dhcp layer=> network
+// ttl => pass with 15 router before work 
+
+//? ping 192.168.1.1 -n 1000 (getway) check ping 192.168.1.1 -t =>ctrl c
+//ping (getway) -l 20000 -t
+//arp table make broadcast to get ip/mac 223 one to one unicast,224=>multicast,255=broadcast
+
+//ip v6 [128bit 8bits]
+// ! tcp transmission control protocol email ,file char ,important
+// ! udp user datagram protocol ,speed video streaming
+
+//netstat /all cmd =>shodan.io
+
+// socket =>port +ip
+//application layer=>
+// url universal resource locator 
+// http hypertext transfer protocol
+//dns domain name server =>godady
+//tlds top level domains
+//dns static any dns =>dhcp could be fake
+//ftp server => for company to upload your data for custom company
+//telnet/ssh connect to remote computer
+//remote desktop protocol rdp => enable 2 protocols
+// mail server =>smtp send mail transfer protocol read wite form client
+//pop3 post office protocol read write download save data on server
+//imap4 internet message access protocol v4 only head of message
+
+//! cyber security
+//cia =>confident,available,integrity
+//security =>risk= threat * vulnerabilites
+//attack =>passive[not thing do know what are you do],active[accept cookies]
+//social engineer=>phishing,hiJack,insider,backdoor,trapdoor,virus,trojan,worms,zombie,ransomware,spam,spyware,tracking cookies
+//change password 42 day
+// have i been pwned
+//firewall data nids,nips check data not heading
+// next generation firewall 
